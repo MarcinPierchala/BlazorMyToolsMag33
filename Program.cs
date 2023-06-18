d907d6e31dd77ce028d8653682641695131034d2
MarcinPierchala <macintecmarcin@gmail.com>
2023-06-18 11:15:14 +0200

    changedapiUrlconfig add_appsettingsJsonfile changerProgramCS

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//configure http client
+builder.Services.AddScoped(x =>
+{
+    var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+    return new HttpClient() { BaseAddress = apiUrl };
+});
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
